842953250e1aad3fae5323b523da5e2871eaad90
Bogdan Popescu <support@kapeli.com>
2012-05-17 20:04:04 +0300

    Everything is !important

--- HighlightedWebView/DHMatchedText.h
+++ HighlightedWebView/DHMatchedText.h
@@ -30,5 +30,5 @@
 
 // static NSString *DHHighlightSpan = @"-webkit-border-radius: 7px; -webkit-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.6); background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(rgba(244, 234, 38, 0.7)), to(rgba(237, 206, 0, 0.7)) ); border: 1px solid rgba(244, 234, 38, 0.8);display:inline;position:static;margin:0px 0px 0px 0px;padding:0px 0px 0px 0px;";
 
-static NSString *DHHighlightSpan = @"background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(rgba(244, 234, 38, 0.7)), to(rgba(237, 206, 0, 0.7)) );display:inline;position:static;margin:0px 0px 0px 0px;padding:0px 0px 0px 0px;opacity:1.0;";
-static NSString *DHSpanWrap = @"display:inline;position:static;margin:0px 0px 0px 0px;padding:0px 0px 0px 0px;opacity:1.0;";
+static NSString *DHHighlightSpan = @"background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(rgba(244, 234, 38, 0.7)), to(rgba(237, 206, 0, 0.7)) ) !important;display:inline !important;position:static !important;margin:0px 0px 0px 0px !important;padding:0px 0px 0px 0px !important;opacity:1.0 !important;";
+static NSString *DHSpanWrap = @"display:inline !important;position:static !important;margin:0px 0px 0px 0px !important;padding:0px 0px 0px 0px !important;opacity:1.0 !important;";
